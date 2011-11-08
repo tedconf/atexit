b2c34df667bce43a7c9b816aed78e08a5dff0b3e
Miki Tebeka <miki.tebeka@gmail.com>
2011-11-08 15:52:14 -0800

    Documentation

--- atexit.go
+++ atexit.go
@@ -1,3 +1,5 @@
+// Simple atexit implementation. Add handlers using Register, you must call
+// atexit.Exit to get the handler invoked (and then terminate the program).
 package atexit
 
 import (
@@ -6,6 +8,7 @@ import (
 
 var handlers = []func(){}
 
+// Exit runs all the atexit handlers and them terminates the program using os.Exit(code)
 func Exit(code int) {
 	for _, handler := range handlers {
 		handler()
@@ -14,6 +17,7 @@ func Exit(code int) {
 	os.Exit(code)
 }
 
+// Add atexit handler, call atexit.Exit to invoke all handlers.
 func Register(handler func()) {
 	handlers = append(handlers, handler)
 }
